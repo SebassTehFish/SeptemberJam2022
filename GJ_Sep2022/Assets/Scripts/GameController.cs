fa3e8d4f761a2ea3525fd2c711a30827334cdd01
austin81215 <aumaxx04@gmail.com>
2022-09-17 16:16:19 -0600

    random room number

--- GJ_Sep2022/Assets/Scripts/GameController.cs
+++ GJ_Sep2022/Assets/Scripts/GameController.cs
@@ -26,4 +26,12 @@ public class GameController : MonoBehaviour
     {
         score = newScore;
     }
+
+    public int getRoomNumber()
+    {
+        int number1 = Random.Range(1, 6);
+        int number2 = Random.Range(1, 4);
+
+        return number1 * 100 + number2;
+    }
 }
