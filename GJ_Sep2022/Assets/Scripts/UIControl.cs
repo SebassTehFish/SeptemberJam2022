18fcf967557d7fd0620519bd6feb9924329f6958
AJNaito <89541191+AJNaito@users.noreply.github.com>
2022-09-17 18:56:36 -0600

    UIControl - Added timer, order counter, display order progression

---
+++ GJ_Sep2022/Assets/Scripts/UIControl.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIControl : MonoBehaviour
+{
+    public Slider slide; 
+    [SerializeField]
+    private TextMeshProUGUI timerText;
+
+    private int maxFoodCarry = 7;
+
+    private int[] foodOrders = new int[7];
+    private int currentCount = 0;
+
+    public void updateTime(float curTime)
+    {
+        int minutes = Mathf.FloorToInt(curTime / 60);
+        int seconds = Mathf.FloorToInt(curTime % 60);
+
+        timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+
+    public void FoodbarProgress(float val)
+    {
+        slide.value = val;
+    }
+
+    public void putNewOrder(int roomNumber)
+    {
+        if (foodOrders.Length <= 7)
+        {
+            foodOrders[currentCount] = roomNumber;
+
+            FoodbarProgress((float)currentCount / maxFoodCarry);
+        }
+    }
+
+    public int getOrderCounter()
+    {
+        return currentCount;
+    }
+
+    public void orderFilled(int index)
+    {
+        if (currentCount > 0)
+        {
+            foodOrders[index] = 0;
+        }
+    }
+}
